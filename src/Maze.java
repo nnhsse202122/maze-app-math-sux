1789241046d6ac39f194d9b72945de8e045a05a6
vvvaid <vvvaid@stu.naperville203.org>
2021-10-19 13:55:06 -0500

    SOS, messed up branching, new branch 2.0 for maze skeleton

---
+++ src/Maze.java
@@ -0,0 +1,38 @@
+public class Maze {
+
+    public Maze()
+    {
+
+    }
+
+    boolean loadMaze(String fname)
+    {
+
+    }
+
+    ArrayList<Square> getNeighbors(Square sq)
+    {
+
+
+    }
+
+    Square getStart()
+{
+
+}
+
+    Square getFinish()
+    {
+
+    }
+
+    void reset()
+    {
+
+    }
+    
+    String toString()
+    {
+
+    }
+}
