cfe67e9964ecc0614ceb103aba7ff8b52a39344a
vvvaid <vvvaid@stu.naperville203.org>
2021-10-19 14:15:03 -0500

    Started building maze class

--- src/Maze.java
+++ src/Maze.java
@@ -1,14 +1,30 @@
 public class Maze {
 
     public Maze()
+
+    private Square[][] maze;
+
     {
 
     }
 
     boolean loadMaze(String fname)
+{
+    try
     {
+    File mazeFile = new File(fname);
+    Scanner in = null;
+    
+    //...
 
     }
+catch( IOException exception )
+    {
+    System.out.println( "The specified file: " + fname + " was not found." );
+    return false;
+    }
+
+}
 
     ArrayList<Square> getNeighbors(Square sq)
     {
